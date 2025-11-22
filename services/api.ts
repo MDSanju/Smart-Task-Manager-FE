b2aae7fbdc32cc2112290c9697aeca524e4ee2da
MDSanju <mdsanju387@gmail.com>
2025-11-22 20:34:46 +0600

    Use deployed API

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://smart-task-manager-server.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
